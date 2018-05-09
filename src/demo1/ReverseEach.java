8b36dd8ae0637cfbfcc2b9b4374d70c180f45dc1
hcrashmi <37067788+hcrashmi@users.noreply.github.com>
2018-05-09 12:38:25 +0530

    reversing Each word

---
+++ src/demo1/ReverseEach.java
@@ -0,0 +1,24 @@
+package demo1;
+
+public class ReverseEach {
+	public static void main(String[] args) {
+		String s2="I am here for interview";
+		
+		char[] ch=s2.toCharArray();
+		String res="";
+		for(int i=0;i<ch.length;i++)
+		{
+			String s1="";
+			while(i<ch.length && ch[i]!=' ')
+		{
+			//s1=ch[i]+s1;
+				s1=s1+ch[i];
+			i++;
+		}
+			//res=res+s1+" ";
+			res=s1+res+" ";
+		}
+		
+		System.out.println(res);
+	}
+}
